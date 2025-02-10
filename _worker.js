df5383ff8b671d3b794070889acf64b8faf7eaac
lyqnihao <56381217+lyqnihao@users.noreply.github.com>
2025-02-10 17:45:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,14 +1,14 @@
 
 // 部署完成后在网址后面加上这个，获取自建节点和机场聚合节点，/?token=auto或/auto或
 
-let mytoken = 'auto';
-let guestToken = ''; //可以随便取，或者uuid生成，https://1024tools.com/uuid
-let BotToken = ''; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
-let ChatID = ''; //可以为空，或者@userinfobot中获取，/start
+let mytoken = 'lyqnihao';
+let guestToken = 'guest'; //可以随便取，或者uuid生成，https://1024tools.com/uuid
+let BotToken = '7665606770:AAEFYMjSETNmwWM308FktZq74qzvXHrCo0c'; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
+let ChatID = '5406519512'; //可以为空，或者@userinfobot中获取，/start
 let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
 let FileName = 'CF-Workers-SUB';
-let SUBUpdateTime = 6; //自定义订阅更新时间，单位小时
-let total = 99;//TB
+let SUBUpdateTime = 1; //自定义订阅更新时间，单位小时
+let total = 10;//TB
 let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
@@ -24,7 +24,7 @@ https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub
 
 let urls = [];
 let subConverter = "SUBAPI.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode_WARP.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
